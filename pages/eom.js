b6629e3867e9d1c9412d0846ab4869b9afbd45d5
Raveendra Mokka <raveendra.mokka@stits.co>
2021-05-19 23:54:23 +0100

    Added data source

--- pages/eom.js
+++ pages/eom.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import axios from 'axios';
 
-const Eom = () => {
+const Eom = ({employee}) => {
     return (
-        <div>
+        <div className='page-container'>
             <h2>EMPLOYEE OF THE MONTH</h2>
         </div>
     )
 }
 
+export const getServerSideProps = async (pageContext) =>{
+    const resp = await axios.get('https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth')
+}
 export default Eom;
